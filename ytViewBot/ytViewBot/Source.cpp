3143299ea2296c98630464fa28021f15cbefdcf7
bonn999 <116773192+bonn999@users.noreply.github.com>
2023-01-14 09:13:50 -0800

    Add files via upload
    I have cleaned up the code a bit to make it a little bit pretty. No new function changes as of yet

--- ytViewBot/ytViewBot/Source.cpp
+++ ytViewBot/ytViewBot/Source.cpp
@@ -8,50 +8,51 @@
 #include <string>
 using namespace std;
 
-void openChrome(string url) {
-    // Start a new background instance of Chrome
+void openW(string url) { // import string url then pass command in prompt window to start chrome
     string command = "start /B chrome.exe " + url;
     system(command.c_str());
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+}
 
-    // Simulate key press to play the video
+void start() { // sleep for 2 seconds and press the space bar to start the video
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     INPUT input;
     input.type = INPUT_KEYBOARD;
-    input.ki.wVk = VK_SPACE; // VK_SPACE is the virtual key code for the spacebar
-    input.ki.dwFlags = 0; // 0 for key press, KEYEVENTF_KEYUP for key release
+    input.ki.wVk = VK_SPACE;
+    input.ki.dwFlags = 0;
     SendInput(1, &input, sizeof(INPUT));
+}
 
-    // Minimize the chrome window
+void MinW() { // pass keyboard events to window to minimize window
     keybd_event(VK_LWIN, 0x5B, 0, 0);
     keybd_event(VK_DOWN, 0x28, 0, 0);
     keybd_event(VK_LWIN, 0x5B, KEYEVENTF_KEYUP, 0);
     keybd_event(VK_DOWN, 0x28, KEYEVENTF_KEYUP, 0);
-
 }
 
-void closeChrome() {
+void closeW() { // pass command to prompt to kill chrome
     system("taskkill /F /IM chrome.exe");
 }
 
 int main() {
-    string url;
-    int min_seconds, max_seconds, loop;
-    cout << "Enter the URL you want to open: " << endl << endl;
+    string url, ipPath;
+    int min, max, loop;
+    cout << "Enter the url " << endl;
     cin >> url;
-    cout << "Please enter the min time to watch video in seconds and then enter the max time" << endl << endl;
-    cin >> min_seconds;
-    cin >> max_seconds;
-    cout << "From " << min_seconds << " to " << max_seconds << "selected." << endl << endl;
-    cout << "Enter the number of loops/views: " << endl << endl;
+    cout << "Enter the minimum time " << endl;
+    cin >> min;
+    cout << "Enter the maximum time " << endl;
+    cin >> max;
+    cout << "From " << min << " to " << max << "selected." << endl;
+    cout << "Enter path to the ip list to use " << endl;
+    cin >> ipPath;
+    cout << "Enter the amout of loops/views " << endl;
     cin >> loop;
-    string filepath;
-    cout << "Enter path of the file containing the IP addresses: " << endl << endl;
-    cin >> filepath;
+    int winM;
+    cout << "Do you wish to minimize the window after it opens?" << endl << endl << "Yes = 1 No = 0";
+    cin >> winM;
     cout << endl << endl;
     vector<string> IPs;
-
-    // Open the file and read the IP addresses into the vector
-    ifstream file(filepath);
+    ifstream file(ipPath);
     if (file.is_open()) {
         string ip;
         while (getline(file, ip)) {
@@ -60,26 +61,42 @@ int main() {
         file.close();
     }
     else {
-        cout << "Unable to open the file at the specified path." << endl;
+        cout << "Unable to open file at specified path." << endl;
         return 0;
     }
-
     for (int i = 0; i < loop; i++) {
         if (IPs.empty()) {
             cout << "No more IPs" << endl;
             break;
         }
         string newIP = IPs.back();
-        IPs.pop_back();
-        string command = "netsh interface ipv4 set address \"Local Area Connection\" static " + newIP + " 255.255.255.0 192.168.1.1";
-        system(command.c_str());
-        openChrome(url);
-        int random_seconds = rand() % (max_seconds - min_seconds + 1) + min_seconds;
-        std::this_thread::sleep_for(std::chrono::seconds(random_seconds));
-        closeChrome();
+        IPs.pop_back(); // this will change the internet settings   interface used                       ip from list     mask        gateway   ---- You can chage whatever you want here
+        string command = "netsh interface ipv4 set address name=\"Local Area Connection\" source=static " + newIP + " 255.255.255.0 192.168.1.1";
+        system(command.c_str()); // just remember line 92 is to reset your settings back to original, be sure to run program all the way through or you will have to manually reset
+        openW(url); //              your internet settings, if you have to manually reset use commands in line 92 to do so
+        start();
+        if (winM > 0) {
+            MinW();
+            break;
+        }
+        int random = rand() % (max - min + 1) + min;
+        std::this_thread::sleep_for(std::chrono::seconds(random));
+        closeW();
     }
-    // This will restore your ip back to original     this can be changed if you wish     right here <--- set your original ip to reset it back to normal whe the program has finished
-    string command2 = "netsh interface ipv4 set address \"Local Area Connection\" static 'Place Ip here' 255.255.255.0 192.168.1.1";
-    system(command2.c_str());
+                  // resets your original settings            place original interface                 original IP,      Mask       Gateway
+    string command2 = "netsh interface ipv4 set address name=\"Local Area Connection\" source=static 'Place Ip here' 255.255.255.0 192.168.1.1";
+    system(command2.c_str()); // if you were using DHCP and want that back on use command "netsh interface ipv4 set address name=\"Local Area Connection\" source=dhcp"
     return main();
 }
+/*
+use command prompt with command
+    
+    -- netsh interface ipv4 show address
+    
+"With whatever interface you are useing you will place the interface in"
+    
+    -- /"INTERFACE-HERE/"
+
+if DHCP is on and program is showing an error trying to use the command make sure this program is being ran in administrator mode
+if it still didnt work manually config your network settings to make it take a static ip
+*/
